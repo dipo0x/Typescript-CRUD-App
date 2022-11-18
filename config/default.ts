76686249512858a124841f0b5fe5822d69e469d7
adesiyanoladipo <adesiyanoladipo1@gmail.com>
2022-11-18 16:47:54 +0100

    ref: refactored config

--- config/default.ts
+++ config/default.ts
@@ -5,5 +5,11 @@ export default {
 	port: process.env.PORT,
 	host: process.env.HOST,
 	dbUri: process.env.MONGO_URI,
-	saltWorkFactor: process.env.saltWorkFactor
+	saltWorkFactor: process.env.saltWorkFactor,
+	database_type: process.env.DATABASE_TYPE,
+	database_host: process.env.DATABASE_HOST,
+	database_port: process.env.DATABASE_HOST,
+	database_username: process.env.DATABASE_USERNAME,
+	database_password: process.env.DATABASE_PASSWORD,
+	database_name: process.env.DATABASE_NAME,
 }
--- src/data-source.ts
+++ src/data-source.ts
@@ -4,14 +4,21 @@ import { DataSource } from "typeorm";
 import { User } from "./entity/User";
 import { Post } from "./entity/Post";
 
-const mongo_uri = config.get("dbUri") as string;
+const database_type = config.get("database_type") as string;
+const database_host = config.get("database_host") as string;
+const database_port = config.get("database_port") as number;
+const database_username = config.get("database_username") as string;
+const database_password = config.get("database_password") as string;
+const database_name = config.get("database_name") as string;
 
 export const AppDataSource = new DataSource({
-    type: "mongodb",
-    url: mongo_uri,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    synchronize: true,
-    logging: false,
-    entities: [User, Post],
+    "type": database_type as any, 
+    "host": database_host, 
+    "port": database_port, 
+    "username": database_username, 
+    "password": database_password, 
+    "database": database_name, 
+    "synchronize": true, 
+    "logging": false,
+    "entities": [User, Post],
 })
